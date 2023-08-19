d42eef63a785b28ec1f71aa7eca8f7547d40092b
coramarcu <cora.marcu@gmail.com>
2023-08-19 15:32:21 +0100

    move quersies in separate folder

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -5,7 +5,7 @@ export default function ClientRow({ client }) {
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
-      <td>{client.phome}</td>
+      <td>{client.phone}</td>
       <td>
         <button className="btn btn-danger btn-sm">
           <FaTrash />
