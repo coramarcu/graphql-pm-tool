9ae169c4de916109a58a3162d629a669278ab2c6
coramarcu <cora.marcu@gmail.com>
2023-08-19 15:15:22 +0100

    create Clients table

--- server/index.js
+++ server/index.js
@@ -4,12 +4,15 @@ require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const connectDB = require("./config/db");
+const cors = require("cors");
 const port = process.env.PORT || 8080;
 
 const app = express();
 
 // Connect to database
 connectDB();
+
+app.use(cors());
 
 app.use(
   "/graphql",
